75ea945a50e67e6b65af10750910a62ac8b83dc7
rksvw <ritik.vw98@gmail.com>
2025-06-13 11:42:10 +0530

    first commit

---
+++ src/app/components/expense-entry/expense-entry.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-entry',
+  standalone: true,
+  imports: [],
+  templateUrl: './expense-entry.component.html',
+  styleUrl: './expense-entry.component.css'
+})
+export class ExpenseEntryComponent {
+  title: any;
+  constructor() {}
+  ngOnInit(): void {
+    this.title = "Expense Entry"
+  }
+}
